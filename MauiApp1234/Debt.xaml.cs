fed148f14e927fc67d8000cc8cee509fa036f939
b66855mm <b66855mm@H-C10BS4005035>
2025-04-04 17:34:43 +0100

    added some pages

---
+++ MauiApp1234/Debt.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1234;
+
+public partial class Debt : ContentPage
+{
+	public Debt()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiApp1234/Investing.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1234;
+
+public partial class Investing : ContentPage
+{
+	public Investing()
+	{
+		InitializeComponent();
+	}
+}
