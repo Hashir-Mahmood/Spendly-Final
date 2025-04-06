da31b282c7e41a7bafc97c2bde7abcccf11b16ab
Bruno-2005 <bgc102005@gmail.com>
2025-04-07 00:12:07 +0100

    Added C# navigation code for InfoHub1

--- MauiApp1234/InfoHub1.xaml.cs
+++ MauiApp1234/InfoHub1.xaml.cs
@@ -1,9 +1,49 @@
+using System.Collections.ObjectModel;
+
 namespace MauiApp1234;
 
 public partial class InfoHub1 : ContentPage
 {
-	public InfoHub1()
-	{
-		InitializeComponent();
-	}
+
+    public InfoHub1()
+    {
+        InitializeComponent();
+        BindingContext = new InfoHubViewModel();
+    }
+    public class Article
+    {
+        public string Title { get; set; }
+        public string URL { get; set; }
+    }
+
+    public class InfoHubViewModel
+    {
+        public ObservableCollection<Article> Articles { get; set; }
+
+        public InfoHubViewModel()
+        {
+            Articles = new ObservableCollection<Article>
+        {
+            new Article { Title = "Budgeting Basics", URL = "https://www.bbc.co.uk/" },
+            new Article { Title = "Investing 101", URL = "https://www.bbc.co.uk/" },
+            new Article { Title = "Saving Tips", URL = "https://www.bbc.co.uk/" }
+        };
+        }
+    }
+
+
+    private void Quiz_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new MainPage());
+    }
+
+    private void Category_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new InfoHub2());
+    }
+
+    private void SignOut_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new LogInPage1());
+    }
 }
