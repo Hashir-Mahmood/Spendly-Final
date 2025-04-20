9fdff5856d4c8a9bb07fd73cc5cf22248bc02fa2
Hashir <thatburntnerve@gmail.com>
2025-04-20 20:36:48 +0100

    added syncfusion license key

--- MauiApp1234/App.xaml.cs
+++ MauiApp1234/App.xaml.cs
@@ -5,6 +5,7 @@
         public App()
         {
             InitializeComponent();
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NNaF5cXmtCe0x0RXxbf1x1ZFRHalxVTnRdUiweQnxTdEBjXX1ecXZWQ2VfVUJyW0lfag==");
 
             MainPage = new AppShell();
         }
