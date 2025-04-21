1bfaa0dcf6ba89907fba4ec7297540d7c625532b
Hashir <thatburntnerve@gmail.com>
2025-04-21 13:34:01 +0100

    maybe fix

--- MauiApp1234/Pages/Budgeting/Budgeting.xaml.cs
+++ MauiApp1234/Pages/Budgeting/Budgeting.xaml.cs
@@ -2,22 +2,38 @@ namespace MauiApp1234;
 using MySqlConnector;
 public partial class Budgeting : ContentPage
 {
-	public Budgeting()
-	{
-		InitializeComponent();
-        string connString = "server=dbhost.cs.man.ac.uk;user=b66855mm;password=iTIfvSknLwQZHtrLaHMy4uTsM/UuEQvZfTqa0ei81+k;database=b66855mm";
-        MySqlConnection conn = new MySqlConnection(connString);
-        conn.Open();
+    private readonly string connString = "server=dbhost.cs.man.ac.uk;user=b66855mm;password=iTIfvSknLwQZHtrLaHMy4uTsM/UuEQvZfTqa0ei81+k;database=b66855mm";
+
+    public Budgeting()
+    {
+        InitializeComponent();
+        LoadData();
+    }
+
+    private async void LoadData()
+    {
+        try
+        {
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                await conn.OpenAsync();
+                // Do your database operations here
+
+                // The connection will automatically close when the using block ends
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Database Error", ex.Message, "OK");
+        }
     }
 
     private void ChatbotIcon_Tapped(object sender, TappedEventArgs e)
     {
-
     }
 
     private void OnNotificationsIconTapped(object sender, TappedEventArgs e)
     {
-
     }
 
     private void SettingsIcon_Tapped(object sender, TappedEventArgs e)
