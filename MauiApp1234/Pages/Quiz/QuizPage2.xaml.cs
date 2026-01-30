97cceb5cc2bed5fb413d67237ccd26bc6d48a387
Muhammad Hashir Mahmood <thatburntnerve@gmail.com>
2026-01-30 12:11:08 +0000

    Remove database connection string from QuizPage2
    Removed sensitive database connection string.

--- MauiApp1234/Pages/Quiz/QuizPage2.xaml.cs
+++ MauiApp1234/Pages/Quiz/QuizPage2.xaml.cs
@@ -5,7 +5,7 @@ public partial class QuizPage2 : ContentPage
 	public QuizPage2()
 	{
 		InitializeComponent();
-        string connString = "server=dbhost.cs.man.ac.uk;user=b66855mm;password=iTIfvSknLwQZHtrLaHMy4uTsM/UuEQvZfTqa0ei81+k;database=b66855mm";
+        string connString = "";
         MySqlConnection conn = new MySqlConnection(connString);
         conn.Open();
     }
